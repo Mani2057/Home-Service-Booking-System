3f070d25bb3e6c48d123ae151a464bb06a2035e4
Maniram Yadav <maniram@Manirams-MacBook-Air.local>
2025-04-06 18:26:24 +0530

    worked on UI

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 
 const app = express();
-
+ 
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-// Session Middleware
+// Session Middleware ;l'
+
 app.use(session({
   secret: "secretKey",
   resave: false,
